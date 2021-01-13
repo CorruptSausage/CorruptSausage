d28739cb58ab813f5d570a8b4e5c4b987eb47049
CorruptSausage <Slaats.Jordy@outlook.com>
2021-01-13 15:23:10 +0100

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -1,3 +1,5 @@
+const { MessageEmbed, version } = require("discord.js");
+
 module.exports = {
     name: 'status',
     description: 'status command',
@@ -12,7 +14,14 @@ module.exports = {
                 return message.reply(`please wait ${timeLeft.toFixed(1)} seconds before using this command again`);
             }
         } else {
-            message.channel.send(`Bot is online and responding to commands.`)
+            const statusEmbed = new MessageEmbed()
+            .setTitle('Bot Status:')
+            .setColor('#FF0000')
+            .addField('Bot Status:', 'Online and responding to commands.')
+            .addField('Bot Version:', version)
+            .setFooter('CorruptSausage™')
+
+            message.channel.send(statusEmbed);
             timeout.add(message.author.id)
             setTimeout(() => {
                 timeout.delete(message.author.id)
